ef8b184f4ce19aa2f22b711cbbd6859d82624430
Stefan <stefan.fraenkel@gmail.com>
2022-01-24 20:54:07 +0100

    Update web.php
    routes for registering as guests

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WdController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/master', function () {
+    return view('wd-master');
+});
+
+Route::post('/register1', [WdController::class, 'register1'])->name('register1');
+Route::post('/register', [WdController::class, 'createVisitor'])->name('createVisitor');
+
+
+Route::get('/test', [WdController::class, 'test']);
+
 
 Route::get('/{locale?}', function ($locale = null) {
     if (isset($locale) && in_array($locale, config('app.available_locales'))) {
