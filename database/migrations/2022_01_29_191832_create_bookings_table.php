617d762de2e048154910653ce0f24b13d38fc838
Stefan <stefan.fraenkel@gmail.com>
2022-01-30 00:16:38 +0100

    Create 2022_01_29_191832_create_bookings_table.php

---
+++ database/migrations/2022_01_29_191832_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('adult_guests');
+            $table->integer('child_guests');
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->string('room_type');
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
