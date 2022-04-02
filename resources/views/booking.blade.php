ca39a82a8b39d28b10b95a096e047a07b5ae5c17
gitlab-ci <stefan.fraenkel@gmail.com>
2022-04-02 18:43:08 +0200

    Update booking.blade.php

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -26,19 +26,6 @@
 	//////////////////////////////////////////////////////
 	 -->
 
-
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
 	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
 	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
 
@@ -76,10 +63,29 @@
 
 	<div class="fh5co-loader"></div>
 
-	<div id="page">
-
-<div id="attending"></div>
+    <div id="page">
+        <nav class="fh5co-nav" role="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-2 menu-1">
+                        <div id="fh5co-logo"><a href="index.html">{{ __('Hochzeit') }}<strong>.</strong></a></div>
+                    </div>
+                    <div class="col-xs-10 text-right menu-1">
+                        <ul>
+                            <li><a href="index.html">{{ __('Start') }}</a></li>
+                            @auth
+                                <li class="active"><a href="/profile">{{ __('Mein Profil') }}</a></li>
+                                <li><a href="/wd-logout">{{ __('Abmelden') }}</a>
+                            @else
+                                <li><a href="/profile">{{ __('Anmelden') }}</a></li>
+                            @endauth
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
 	<div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/img_bg_3.jpg);">
+        <br>
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row animate-box">
